925aec2d7871569df987d711aaf9140063cb1c98
d.dumchev <d.dumchev@supportix.ru>
2024-01-14 17:43:51 +0400

    таймер

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>Hello</h1>
+  <div class="wrapper">
+    <div class="header"></div>
+    <div class="content">
+      <TimeString />
+    </div>
+    <div class="footer"></div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+  import TimeString from "@/components/TimeString.vue";
+</script>
+
+<style>
+  .wrapper {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .content {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
