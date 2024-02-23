383eafb8223e2c0afc54cbd76540e24896d13ffa
ddum <ddum.88@mail.ru>
2024-02-23 18:36:06 +0400

    Удалил подключение withDefaults

--- src/components/LinksList.vue
+++ src/components/LinksList.vue
@@ -12,8 +12,6 @@
 </template>
 
 <script lang="ts" setup>
-import { withDefaults } from 'vue'
-
 import LinkFavicon from '@/components/element/LinkFavicon.vue'
 import type { LinkItem } from '@/stores/types'
 
