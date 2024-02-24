1d7bfb043296b61ca31af21d5fe246feb69bf404
ddum <ddum.88@mail.ru>
2024-02-24 10:45:01 +0400

    поправил тест InputBase

--- src/components/element/__tests__/InputBase.test.ts
+++ src/components/element/__tests__/InputBase.test.ts
@@ -15,16 +15,16 @@ test('render InputBase', () => {
     `"<input data-v-56868875="" type="text" class="input" placeholder="Placeholder Text">"`
   )
 
-  const input = wrapper.find('.input')
-  expect((input.element as HTMLInputElement).value).toBe('Input Text')
+  const input = wrapper.find<HTMLInputElement>('.input')
+  expect(input.element.value).toBe('Input Text')
 })
 
 test('InputBase ввод текста', async () => {
   const wrapper = mount(InputBase)
-  const input = wrapper.find('.input')
+  const input = wrapper.find<HTMLInputElement>('.input')
 
   await input.setValue('Test Text')
-  expect((input.element as HTMLInputElement).value).toBe('Test Text')
+  expect(input.element.value).toBe('Test Text')
 
   expect(wrapper.emitted()).toHaveProperty('update:modelValue')
 })
