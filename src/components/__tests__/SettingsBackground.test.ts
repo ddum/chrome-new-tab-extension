a1646edca669fda9184ed79b268ab81ed957a08e
ddum <ddum.88@mail.ru>
2024-02-25 14:17:05 +0400

    поправил тест SettingsBackground

--- src/components/__tests__/SettingsBackground.test.ts
+++ src/components/__tests__/SettingsBackground.test.ts
@@ -1,12 +1,29 @@
-import { test, expect } from 'vitest'
+import { test, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import { setActivePinia, createPinia } from 'pinia'
+import { createTestingPinia } from '@pinia/testing'
 import { nextTick } from 'vue'
 
 import SettingsBackground from '../SettingsBackground.vue'
 import { useAppStore } from '../../stores'
 
 test('render пустой SettingsBackground', async () => {
+  const wrapper = mount(SettingsBackground, {
+    global: {
+      plugins: [
+        createTestingPinia({
+          createSpy: vi.fn
+        })
+      ]
+    }
+  })
+  expect(wrapper.findComponent({ name: 'FormAddTag' }).exists()).toBe(true)
+  expect(wrapper.find('.tags').exists()).toBe(true)
+
+  expect(wrapper.findAllComponents({ name: 'TagBase' })).toHaveLength(0)
+})
+
+test('render SettingsBackground', async () => {
   setActivePinia(createPinia())
 
   const wrapper = mount(SettingsBackground)
@@ -16,6 +33,5 @@ test('render пустой SettingsBackground', async () => {
   appStore.addTag('test2')
 
   await nextTick()
-  expect(wrapper.findComponent({ name: 'FormAddTag' }).exists()).toBe(true)
   expect(wrapper.findAllComponents({ name: 'TagBase' })).toHaveLength(2)
 })
